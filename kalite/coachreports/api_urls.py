50e35899e1e42869f225738ca6c31f45487912bb
Jamie Alexandre <jamalex@gmail.com>
2013-06-16 19:32:13 -0700

    Allow topics to be selected via topic tree on graph pages.

--- kalite/coachreports/api_urls.py
+++ kalite/coachreports/api_urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('coachreports.api_views',
     url(r'data/$',      'api_data', {}, 'api_data'),
     url(r'^data/(?P<xaxis>\w+)/(?P<yaxis>\w+)/$', 'api_data', {}, 'api_data_2'),
 #    url(r'friendly/$',  'api_friendly_names', {}, 'api_friendly_names'),
+    url(r'^get_math_topic_tree', 'get_math_topic_tree', {}, 'get_math_topic_tree')
 )
 
