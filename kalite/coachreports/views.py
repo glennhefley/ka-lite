56e03667b3b656c769d9c775008668dac6bca48d
Ben Cipollini <ben@learningequality.org>
2013-06-17 16:17:03 -0700

    adding student view

--- kalite/coachreports/views.py
+++ kalite/coachreports/views.py
@@ -1,7 +1,9 @@
 import json
 import requests
+import datetime
 from annoying.decorators import render_to
 from annoying.functions import get_object_or_None
+from functools import partial
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, HttpResponseNotFound, HttpResponseServerError
@@ -17,24 +19,26 @@ from shared.views import group_report_context
 from coachreports.forms import DataForm
 from main import topicdata
 from coachreports.api_views import StatusException, get_data_form, stats_dict
+from utils.topic_tools import get_topic_exercises, get_topic_videos
 
-
-"""
-def get_api_data(request, form):
+def get_api_data(request, form=None, form_data=None):
     api_url = "http%s://%s%s" % ("s" if request.is_secure() else "", request.get_host(), reverse("coachreports.api_views.api_data"))
 
-    form = get_data_form(request)
+    if not form_data:
+        if not form:
+            form = get_data_form(request)
+        form_data = form.data
     
     # Make the api request on the server-side,
     #   this is a good way to test the API while under development
     #   (rather than calling the function directly)
-    response = requests.post(api_url, data=form.data)
+    response = requests.post(api_url, data=form_data)
     if response.status_code != 200:
         raise StatusException(message=response.text, status_code=response.status_code)
 
     data = json.loads(response.text)
     return data 
-"""
+
 
 #@require_admin
 @require_login
@@ -88,8 +92,61 @@ def timeline_view(request, facility, xaxis="", yaxis=""):
 @facility_required
 @render_to("coachreports/student_view.html")
 def student_view(request, facility, xaxis="pct_mastery", yaxis="ex:attempts"):
-    context = scatter_view_context(request, facility=facility, xaxis=xaxis, yaxis=yaxis)
-    return context
+    user = request.session["facility_user"]
+    
+    topics = get_all_topics() # piss poor topics 
+    topic_ids = [t['id'] for t in topics]
+    topics = filter(partial(lambda n,ids: n['id'] in ids, ids=topic_ids), topicdata.NODE_CACHE['Topic'].values()) # real data, like paths
+    
+    exercise_logs = dict()
+    video_logs = dict()
+    exercise_sparklines = dict()
+    stats = dict()
+    for topic in topics:
+
+        topic_exercises = get_topic_exercises(path=topic['path'])
+        n_exercises = len(topic_exercises)
+        exercise_logs[topic['id']] = ExerciseLog.objects.filter(user=user, exercise_id__in=[t['name'] for t in topic_exercises]).order_by("completion_timestamp")
+        n_exercises_touched = len(exercise_logs[topic['id']])
+
+
+        topic_videos = get_topic_videos(topic_id=topic['id'])
+        n_videos = len(topic_videos)
+        video_logs[topic['id']] = VideoLog.objects.filter(user=user, youtube_id__in=[tv['youtube_id'] for tv in topic_videos]).order_by("completion_timestamp")
+        n_videos_touched = len(video_logs[topic['id']])
+        
+        exercise_sparklines[topic['id']] = [el.completion_timestamp for el in filter(lambda n: n.complete, exercise_logs[topic['id']])]
+        
+        stats[topic['id']] = {
+            "pct_started":      0 if not n_exercises_touched else n_exercises_touched/float(n_exercises),
+            "average_points":   0 if not n_exercises_touched else sum([el.points for el in exercise_logs[topic['id']]])/float(n_exercises_touched),
+            "average_attempts": 0 if not n_exercises_touched else sum([el.attempts for el in exercise_logs[topic['id']]])/float(n_exercises_touched),
+            "pct_mastery":      0 if not n_exercises_touched else sum([el.complete for el in exercise_logs[topic['id']]])/float(n_exercises_touched),
+            "total_struggling": 0 if not n_exercises_touched else sum([el.struggling for el in exercise_logs[topic['id']]]),
+            "last_completed":None if not n_exercises_touched else max([el.completion_timestamp or datetime.datetime(year=1900, month=1, day=1) for el in exercise_logs[topic['id']]]),
+        }
+
+    context = scatter_view_context(request, facility)
+    return {
+        "form": context["form"],
+        "groups": context["groups"],
+        "facilities": context["facilities"],
+        "user": user,
+        "topics": topics,
+        "topic_ids": topic_ids,
+        "exercise_logs": exercise_logs,
+        "video_logs": video_logs,
+        "exercise_sparklines": exercise_sparklines,
+        "stats": stats,
+        "stat_defs": [
+            {"key": "pct_started",      "title": "% Started",        "type": "pct"},
+            {"key": "average_points",   "title": "Average Points",   "type": "float"},
+            {"key": "average_attempts", "title": "Average Attempts", "type": "float"},
+            {"key": "pct_mastery",      "title": "% Mastery",        "type": "pct"},
+            {"key": "total_struggling", "title": "Total Struggling", "type": "int"},
+            {"key": "last_completed", "title": "Last Completed", "type": "date"},
+        ]
+    }
 
 @require_login
 @facility_required
@@ -122,17 +179,20 @@ def landing_page(request, facility):
 def test(request):
     return {}
 
+def get_all_topics():
+    topics = topicdata.EXERCISE_TOPICS["topics"].values()
+    topics = sorted(topics, key = lambda k: (k["y"], k["x"]))
+    return topics
+
 @facility_required
-@render_to("coachreports/old2.html")
+@render_to("coachreports/table_view.html")
 def old_coach_report(request, facility, report_type="exercise"):
     import re, settings
     from utils.topic_tools import get_topic_videos, get_topic_exercises
     
     # Get a list of topics (sorted) and groups
-    topics = topicdata.EXERCISE_TOPICS["topics"].values()
-    topics = sorted(topics, key = lambda k: (k["y"], k["x"]))
+    topics = get_all_topics()
     groups = FacilityGroup.objects.filter(facility=facility)
-    paths = dict((key, val["path"]) for key, val in topicdata.NODE_CACHE["Exercise"].items())
     context = {
         "report_types": ("exercise","video"),
         "request_report_type": report_type,
@@ -181,7 +241,7 @@ def old_coach_report(request, facility, report_type="exercise"):
 
     elif report_type=="video":
         # Fill in videos
-        context["videos"] = get_topic_videos(topic_id)
+        context["videos"] = get_topic_videos(topic_id=topic_id)
 
         # More code, but much faster
         video_ids = [vid["youtube_id"] for vid in context["videos"]]
